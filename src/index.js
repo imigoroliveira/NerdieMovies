50a370e62bcb607a5e0586f7a05b585fea0e1b6d
Gustavo Soares Reis <gustavosoaresreis1@gmail.com>
2023-04-26 15:17:20 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -13,3 +13,7 @@ raiz . renderizar (
     < Aplicativo  / >
   < / Reagir . Modo estrito >
 ) ;
+
+// Se você deseja começar a medir o desempenho em seu aplicativo, passe uma função
+// para registrar os resultados (por exemplo: reportWebVitals(console.log))
+// ou envie para um endpoint analítico. Saiba mais: https://bit.ly/CRA-vitals
