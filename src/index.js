426d06a85ec3e8f409309bfe99561b33da3cd159
Gustavo Soares Reis <gustavosoaresreis1@gmail.com>
2023-04-26 15:17:09 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,19 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import 'bootstrap/dist/css/bootstrap.css';
-import './index.css';
+importar  Reagir  de  'reagir' ;
+importar  ReactDOM  de  'react-dom/client' ;
+import  'bootstrap/dist/css/bootstrap.css' ;
+importar  './index.css' ;
 
 // Componentes
-import App from './App';
+importar  App  de  './App' ;
 
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode> 
-    <App />
-  </React.StrictMode>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+ raiz  const =  ReactDOM . createRoot ( document . getElementById ( 'raiz' ) ) ;
+raiz . renderizar (
+  < Reagir . Modo estrito > 
+    < Aplicativo  / >
+  < / Reagir . Modo estrito >
+) ;
