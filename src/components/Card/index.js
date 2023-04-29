deb478d5d5bd0a316b2b39923b2c9dcdbe1faa40
Igor Oliveira <igormat4us@gmail.com>
2023-04-29 15:03:00 -0300

    [IMRO] - Adicao de funcionalidade de pesquisa de filmes

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -10,6 +10,7 @@ function Assitido({ javisto }) {
 
 
 export default function Card() {
+  const [allMovies, setAllMovies] = useState([]);
   const [movies, setMovies] = useState([]);
 
   useEffect(() => {
@@ -17,32 +18,54 @@ export default function Card() {
       .then(response => response.json())
       .then(data => {
         const moviesWithIds = data.map((movie, index) => ({ ...movie, id: index }));
+        setAllMovies(moviesWithIds);
         setMovies(moviesWithIds);
       });
   }, []);
 
+  const handleSearch = (event) => {
+    const query = event.target.value.toLowerCase();
+    const filteredMovies = allMovies.filter((movie) => {
+      return (
+        (movie.titulo && movie.titulo.toLowerCase().includes(query)) ||
+        (movie.ano && movie.ano.toString().includes(query)) ||
+        (movie.descricao && movie.descricao.toLowerCase().includes(query))
+      );
+    });
+    setMovies(filteredMovies);
+  }
 
   return (
     <div className="container text-center">
-    <div className="row movie-row" style={{flexWrap: "wrap"}}>
-      {movies.map(movie => (
-        <div className="col-4 mb-4" key={movie.id}>
-          <div className="card">
-            <img src={movie.poster} height="550px" alt={movie.nome} className="card-img-top" />
-            <div className="card-body">
-              <h5 className="card-title">{movie.titulo} ({movie.ano}) </h5>
-              <p className="card-text">{movie.descricao ? movie.descricao.substring(0, 120) + "..." : ""}</p>
-              <a href={`/detalhes/${movie.id}`}>
-                <div className="btn btn-primary">
-                  Detalhes
-                </div>
-              </a>
-              <Assitido javisto={movie.assistido}></Assitido>
+      <div className="row">
+        <div className="col">
+          <input 
+            type="text" 
+            className="form-control mb-4" 
+            placeholder="Pesquisar filmes"
+            onChange={handleSearch}
+          />
+        </div>
+      </div>
+      <div className="row movie-row" style={{flexWrap: "wrap"}}>
+        {movies.map(movie => (
+          <div className="col-4 mb-4" key={movie.id}>
+            <div className="card">
+              <img src={movie.poster} height="550px" alt={movie.nome} className="card-img-top" />
+              <div className="card-body">
+                <h5 className="card-title">{movie.titulo} ({movie.ano}) </h5>
+                <p className="card-text">{movie.descricao ? movie.descricao.substring(0, 120) + "..." : ""}</p>
+                <a href={`/detalhes/${movie.id}`}>
+                  <div className="btn btn-primary">
+                    Detalhes
+                  </div>
+                </a>
+                <Assitido javisto={movie.assistido}></Assitido>
+              </div>
             </div>
           </div>
-        </div>
-      ))}
+        ))}
+      </div>
     </div>
-  </div>
   )
 }
